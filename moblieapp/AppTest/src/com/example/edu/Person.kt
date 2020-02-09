66ce112d765d885a912d13f348022776bac76fe4
SeonSikYun <suns2131@naver.com>
2020-02-09 23:26:26 +0900

    Apptest upload2

---
+++ moblieapp/AppTest/src/com/example/edu/Person.kt
@@ -0,0 +1,7 @@
+package com.example.edu
+
+fun main() {
+    val into: String = "안녕하세요."
+    val num: Int = 20;
+     println("intro: $into, num: $num")
+}
--- moblieapp/AppTest/src/test1.kt
+++ moblieapp/AppTest/src/test1.kt
@@ -1,3 +1,4 @@
 fun main() {
     println("hello kotlin");
+    println("안녕하세요 테스트에요.")
 }
